0c2d484a77d883927bc9b9e9ec9b758acf7d79d8
chardelyce <chardelyce@gmail.com>
2022-01-01 14:38:04 -0600

    added programs buttons added dos file deleted doom file adding dos in 2 min

--- app/src/main/java/com/example/android98/notepad.java
+++ app/src/main/java/com/example/android98/notepad.java
@@ -2,17 +2,13 @@ package com.example.android98;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class andos extends AppCompatActivity {
+public class notepad extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_andos);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_notepad);
     }
-
-
 }
